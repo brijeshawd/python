f66edbce83f9914ea415afa567ecb1608496fa8c
brijesh-pandey-bits <brijesh.pandey.bits@gmail.com>
2026-04-05 22:23:51 +0530

    MDP-8

---
+++ service.py
@@ -0,0 +1,7 @@
+xml
+<dependency>
+    <groupId>org.apache.tomcat.embed</groupId>
+    <artifactId>tomcat-embed-core</artifactId>
+    <version>11.0.20</version>
+    <scope>compile</scope>
+</dependency>
