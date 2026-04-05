afbbcf5edeaa2f6452cfd642b37181ad2bab578c
Brijesh Pandey <brijeshawd@gmail.com>
2026-04-05 22:32:51 +0530

    Update service.py

--- service.py
+++ service.py
@@ -1,7 +1,1 @@
-xml
-<dependency>
-    <groupId>org.apache.tomcat.embed</groupId>
-    <artifactId>tomcat-embed-core</artifactId>
-    <version>11.0.20</version>
-    <scope>compile</scope>
-</dependency>
+
